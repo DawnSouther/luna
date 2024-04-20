0d607ee498fc49292b9447c1d16ae1b1967327bf
yukdawn <yukdawn@gmail.com>
2024-04-21 00:34:38 +0800

    init

---
+++ src/main/groovy/cn/lm/luna/LunaApplication.groovy
@@ -0,0 +1,13 @@
+package cn.lm.luna
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LunaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LunaApplication, args)
+    }
+
+}
